e90556c543e86f1cb157f2bb9431e9a908896ffd
Enoki-ru <110339199+Enoki-ru@users.noreply.github.com>
2022-08-05 17:33:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#icon').addEventListener('click', function(){
+        document.querySelector('#left-menu').classList.toggle('open')
+    })
+})
+window.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#icon').addEventListener('click', function(){
+        document.querySelector('#sticky-icon').classList.toggle('open')
+    })
+})
