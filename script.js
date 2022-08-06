4770314e8c5fc630ca04be0e1e4d9c7f257830e1
Enoki-ru <110339199+Enoki-ru@users.noreply.github.com>
2022-08-06 10:56:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ window.addEventListener('DOMContentLoaded',function(){
         document.querySelector('#sticky-icon').classList.toggle('open')
     })
 })
+window.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#left-Link').addEventListener('click', function(){
+        document.querySelector('#left-Link').classList.add('left-link')
+    })
+})
+window.addEventListener('DOMContentLoaded',function(){
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+})
